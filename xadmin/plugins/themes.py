4faec910269d5a5f55113ac15eaf87084dcbd2db
Cenk <cenk.math@gmail.com>
2014-05-15 14:56:52 +0300

    Revert "Try to add new html templates"
    This reverts commit c58bde4803e6c16c9f50c3f435b6cd026a5bd276.

--- xadmin/plugins/themes.py
+++ xadmin/plugins/themes.py
@@ -74,37 +74,5 @@ class ThemePlugin(BaseAdminPlugin):
 
         nodes.append(loader.render_to_string('xadmin/blocks/comm.top.theme.html', {'themes': themes, 'select_css': select_css}))
 
-    # Block Views
-    def block_egem_top_navmenu(self, context, nodes):
-
-        themes = [{'name': _(u"Default"), 'description': _(
-            u"Default bootstrap theme"), 'css': self.default_theme},
-            {'name': _(u"Bootstrap2"), 'description': _(u"Bootstrap 2.x theme"),
-            'css': self.bootstrap2_theme}]
-        select_css = context.get('site_theme', self.default_theme)
-
-        if self.user_themes:
-            themes.extend(self.user_themes)
-
-        if self.use_bootswatch:
-            ex_themes = cache.get(THEME_CACHE_KEY)
-            if ex_themes:
-                themes.extend(json.loads(ex_themes))
-            else:
-                ex_themes = []
-                try:
-                    watch_themes = json.loads(urllib.urlopen(
-                        'http://api.bootswatch.com/3/').read())['themes']
-                    ex_themes.extend([
-                        {'name': t['name'], 'description': t['description'],
-                            'css': t['cssMin'], 'thumbnail': t['thumbnail']}
-                        for t in watch_themes])
-                except Exception:
-                    pass
-
-                cache.set(THEME_CACHE_KEY, json.dumps(ex_themes), 24 * 3600)
-                themes.extend(ex_themes)
-
-        nodes.append(loader.render_to_string('xadmin/egem/topnav_menu.html', {'themes': themes, 'select_css': select_css}))
 
 site.register_plugin(ThemePlugin, BaseAdminView)
