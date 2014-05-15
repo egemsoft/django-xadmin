c58bde4803e6c16c9f50c3f435b6cd026a5bd276
Cenk <cenk.math@gmail.com>
2014-05-15 14:54:25 +0300

    Try to add new html templates

--- xadmin/plugins/language.py
+++ xadmin/plugins/language.py
@@ -14,6 +14,12 @@ class SetLangNavPlugin(BaseAdminPlugin):
                 'redirect_to': self.request.get_full_path(),
             }, context_instance=RequestContext(self.request)))
 
+    def block_egem_top_navmenu(self, context, nodes):
+        nodes.append(
+            loader.render_to_string('xadmin/blocks/comm.top.setlang.html', {
+                'redirect_to': self.request.get_full_path(),
+            }, context_instance=RequestContext(self.request)))
+
 if settings.LANGUAGES and 'django.middleware.locale.LocaleMiddleware' in settings.MIDDLEWARE_CLASSES:
     site.register_plugin(SetLangNavPlugin, CommAdminView)
     site.register_view(r'^i18n/', lambda site: 'django.conf.urls.i18n', 'i18n')
