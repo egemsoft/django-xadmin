163a4e4a168995ff4c3ccf81a69f1491bcd7f00b
Cenk <cenk.math@gmail.com>
2014-05-21 10:43:39 +0300

    remove egem_navbar

--- xadmin/plugins/topnav.py
+++ xadmin/plugins/topnav.py
@@ -71,31 +71,5 @@ class TopNavPlugin(BaseAdminPlugin):
         nodes.append(
             loader.render_to_string('xadmin/blocks/comm.top.topnav.html', {'add_models': add_models}))
 
-    def block_egem_navbar(self, context, nodes):
-        add_models = []
-
-        site_name = self.admin_site.name
-
-        if self.global_add_models == None:
-            models = self.admin_site._registry.keys()
-        else:
-            models = self.global_add_models
-        for model in models:
-            app_label = model._meta.app_label
-
-            if self.has_model_perm(model, "add"):
-                info = (app_label, model._meta.module_name)
-                try:
-                    add_models.append({
-                        'title': _('Add %s') % capfirst(model._meta.verbose_name),
-                        'url': reverse('xadmin:%s_%s_add' % info, current_app=site_name),
-                        'model': model
-                    })
-                except NoReverseMatch:
-                    pass
-        menu = get_menu()
-        user = get_user()
-        nodes.append(
-            loader.render_to_string('xadmin/blocks/egem_navbar.html', {'user': user,'menu':menu }))
 
 site.register_plugin(TopNavPlugin, CommAdminView)
